487512e8eb443521c8d9e7f156639362e3864ae9
Pim Vellinga <pimvellinga@gmail.com>
2012-02-09 15:56:55 +0100

    Splash with catch and try

--- src/vk/runner/Runner.java
+++ src/vk/runner/Runner.java
@@ -1,9 +1,15 @@
 package vk.runner;
 
 import vk.main.Main;
+import vk.main.SplashScreen;
 
 public class Runner {
 	public static void main(String[] args){
-		new Main();
-		}
+		
+		SplashScreen splash = new SplashScreen(5000);
+		
+		splash.showSplashAndExit();
+		
+		try { new Main(); } catch (Exception e) { e.getStackTrace(); }
+	}
 }
